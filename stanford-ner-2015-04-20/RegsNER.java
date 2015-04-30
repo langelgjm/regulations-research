cda667e35d3b53549ca2a55c14670ce38dcfad80
Gabriel J. Michael <gabriel.j.michael@gmail.com>
2015-04-30 15:41:48 -0400

    NER extraction and insertion into database for ACA comments

--- stanford-ner-2015-04-20/RegsNER.java
+++ stanford-ner-2015-04-20/RegsNER.java
@@ -7,81 +7,112 @@ import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
 import edu.stanford.nlp.util.Triple;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.io.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+// compile with javac -g -cp "*" RegsNER.java
+// run with java -cp "*:." RegsNER
 
 public class RegsNER {
 
-  public static String db() throws ClassNotFoundException
-  {
-	// load the sqlite-JDBC driver using the current class loader
-	Class.forName("org.sqlite.JDBC");
-	Connection connection = null;
-	String myString = "This String was never altered from its default value!";
-	
-	try
-	{
-	  // create a database connection
-	  connection = DriverManager.getConnection("jdbc:sqlite:/Users/gjm/Documents/_Works in Progress/Regulations/data/hhs_dos_comment_db.sqlite");
-	  Statement statement = connection.createStatement();
-	  statement.setQueryTimeout(30);  // set timeout to 30 sec.
+	public static void main(String[] args) throws ClassNotFoundException {
+		// load the sqlite-JDBC driver using the current class loader
+		Class.forName("org.sqlite.JDBC");
+		Connection connection = null;
 
-	  // statement.executeUpdate("drop table if exists person");
-	  ResultSet rs = statement.executeQuery("SELECT documentId, comment FROM documents WHERE documentId == 'CMS-2012-0031-74661' LIMIT 1");
-	  while(rs.next())
-	  {
-		// read the result set
-		System.out.println("documentId = " + rs.getString("documentId"));
-		System.out.println("comment = " + rs.getString("comment"));
-		myString = rs.getString("comment");
-	  }
-	//String myString = rs.getString("comment");   
-	//System.out.println("return value = " + myString);		    
-    }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory", 
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
-    }
-	return myString;
-  }
+		try
+		{
+			// create a database connection
+			connection = DriverManager.getConnection("jdbc:sqlite:/Users/gjm/Documents/_Works in Progress/Regulations/data/hhs_dos_comment_db.sqlite");
+			connection.setAutoCommit(false);
+			
+			
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-  public static void main(String[] args) throws Exception {
-  
-  	String myString = db();
+			// create entities table if necessary
+			CreateEntityTable(connection);
 
-    String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
+			// statement.executeUpdate("drop table if exists person");
+			ResultSet rs = statement.executeQuery("SELECT documentId, comment FROM documents WHERE docketId == 'CMS-2012-0031'");
+			
+			String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
+			AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
 
-    if (args.length > 0) {
-      serializedClassifier = args[0];
-    }
-
-    AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
-
-	//String[] example = {"Good afternoon Rajat Raina, how are you today?",
-	//                    "I go to school at Stanford University, which is located in California." };
-	String[] myStringArray = {myString};
-	for (String str : myStringArray) {
-	System.out.println(classifier.classifyToString(str));
+			while(rs.next()) {
+				// read the result set
+				List<String[]> entity_word_list = ExtractEntities(classifier, rs.getString("comment"));	
+				for (String[] entity_word : entity_word_list) {
+					InsertEntity(connection, rs.getString("documentId"), entity_word);
+				}
+				
+				connection.commit();
+			}
+		} catch(SQLException e) {
+			// if the error message is "out of memory", 
+			// it probably means no database file is found
+			System.err.println(e.getMessage());
+		} catch(IOException e) {
+			System.err.println(e.getMessage());
+		} catch(Exception e) {
+			System.err.println(e.getMessage());			
+		} finally {
+			try {
+				if(connection != null) connection.close();
+			} catch(SQLException e) {
+				// connection close failed.
+				System.err.println(e);
+			}
+		}
 	}
-  }
 
+	private static List<String[]> ExtractEntities(AbstractSequenceClassifier<CoreLabel> classifier, String text) throws Exception {
+		//System.out.println(classifier.classifyToString(text));
+		List<Triple<String, Integer, Integer>> entity_list = classifier.classifyToCharacterOffsets(text);
+
+		ArrayList<String[]> return_list = new ArrayList();
+		
+		for (Triple<String, Integer, Integer> item : entity_list) {
+			//System.out.println(item.first() + ": " + text.substring(item.second(), item.third()));
+			String[] entity_word = { item.first(), text.substring(item.second(), item.third()) };
+			return_list.add(entity_word);
+		}
+		
+		return return_list;
+		
+	}
+	
+	private static void InsertEntity(Connection connection, String documentId, String[] entity_word) throws Exception {
+		//System.out.println(entity_word[0] + ": " + entity_word[1]);		
+
+		//Statement statement = connection.createStatement();
+		//statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+		PreparedStatement statement = null;
+		String insertString = "INSERT INTO entities (documentId, entityType, entityString) VALUES (?, ?, ?)";
+		statement = connection.prepareStatement(insertString);
+		
+		statement.setString(1, documentId);
+		statement.setString(2, entity_word[0]);
+		statement.setString(3, entity_word[1]);
+
+		statement.executeUpdate();
+		statement.close();
+	}	
+
+	private static void CreateEntityTable(Connection connection) throws Exception {
+		Statement statement = connection.createStatement();
+		statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+		statement.executeUpdate("CREATE TABLE IF NOT EXISTS entities (id INTEGER PRIMARY KEY, documentId INTEGER, entityType TEXT, entityString TEXT, FOREIGN KEY(documentId) REFERENCES documents(documentId))");
+        statement.close();
+        connection.commit();
+	}   
 }
